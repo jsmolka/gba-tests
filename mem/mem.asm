576337b017f67cf01eebf92621c82817adbde849
jsmolka <jsmolka@pm.me>
2020-05-26 19:09:49 +0200

    add memory mirror tests

---
+++ mem/mem.asm
@@ -0,0 +1,30 @@
+format binary as 'gba'
+
+include '../lib/constants.inc'
+include '../lib/macros.inc'
+
+macro m_exit test {
+        mov     r12, test
+        b       eval
+}
+
+header:
+        include '../lib/header.asm'
+
+main:
+        m_test_init
+
+        ; Reset test register
+        mov     r12, 0
+
+        ; Tests start at 1
+        include 'mirrors.asm'
+
+eval:
+        m_vsync
+        m_test_eval r12
+
+idle:
+        b       idle
+
+include '../lib/text.asm'
---
+++ mem/mirrors.asm
@@ -0,0 +1,160 @@
+mirrors:
+        ; Tests for memory mirrors
+
+t001:
+        ; EWRAM mirror
+        mov     r0, 1
+        mov     r1, MEM_EWRAM
+        str     r0, [r1]
+        add     r1, 0x40000
+        ldr     r0, [r1]
+        cmp     r0, 1
+        bne     f001
+
+        b       t002
+
+f001:
+        m_exit  1
+
+t002:
+        ; IWRAM mirror
+        mov     r0, 1
+        mov     r1, MEM_IWRAM
+        str     r0, [r1]
+        add     r1, 0x8000
+        ldr     r0, [r1]
+        cmp     r0, 1
+        bne     f002
+
+        b       t003
+
+f002:
+        m_exit  2
+
+t003:
+        ; Palette mirror
+        mov     r0, 1
+        mov     r1, MEM_PALETTE
+        str     r0, [r1, 0x10]
+        add     r1, 0x400
+        ldr     r0, [r1, 0x10]
+        cmp     r0, 1
+        bne     f003
+
+        b       t004
+
+f003:
+        m_exit  3
+
+t004:
+        ; VRAM mirror 1
+        mov     r0, 1
+        mov     r1, MEM_VRAM
+        str     r0, [r1]
+        add     r1, 0x20000
+        ldr     r0, [r1]
+        cmp     r0, 1
+        bne     f004
+
+        b       t005
+
+f004:
+        m_exit  4
+
+
+t005:
+        ; VRAM mirrors 2
+        mov     r0, 1
+        mov     r1, MEM_VRAM
+        add     r1, 0x10000
+        str     r0, [r1]
+        add     r1, 0x10000
+        ldr     r0, [r1]
+        cmp     r0, 1
+        bne     f005
+
+        b       t006
+
+f005:
+        m_exit  5
+
+t006:
+        ; OAM mirror
+        mov     r0, 1
+        mov     r1, MEM_OAM
+        str     r0, [r1]
+        add     r1, 0x400
+        ldr     r0, [r1]
+        cmp     r0, 1
+        bne     f006
+
+        b       t007
+
+f006:
+        m_exit  6
+
+t007:
+        ; GamePak mirror 1
+        adr     r1, .data
+        add     r1, 0x02000000
+        ldr     r0, [r1]
+        cmp     r0, 1
+        bne     f007
+
+        b       t008
+
+.data:
+        dw      1
+
+f007:
+        m_exit  7
+
+t008:
+        ; GamePak mirror 2
+        adr     r1, .data
+        add     r1, 0x04000000
+        ldr     r0, [r1]
+        cmp     r0, 1
+        bne     f008
+
+        b       t009
+
+.data:
+        dw      1
+
+f008:
+        m_exit  8
+
+t009:
+        ; SRAM mirror 1
+        mov     r0, 1
+        mov     r1, MEM_SRAM
+        strb    r0, [r1]
+        add     r1, 0x8000
+        ldrb    r0, [r1]
+        bne     f009
+
+        b       t010
+
+.sram:
+        ; Fake SRAM save type
+        dw      'SRAM'
+
+f009:
+        m_exit  9
+
+t010:
+        ; SRAM mirror 2
+        mov     r0, 1
+        mov     r1, MEM_SRAM
+        strb    r0, [r1]
+        add     r1, 0x01000000
+        ldrb    r0, [r1]
+        bne     f010
+
+        b       mirrors_passed
+
+f010:
+        m_exit  10
+
+mirrors_passed:
